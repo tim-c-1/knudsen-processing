6ab3e58a6f3e329bf014dd5eacbc273f66d33ae4
Tim <153627100+tim-c-1@users.noreply.github.com>
2025-07-02 16:26:15 -0400

    Update line to dataframe copy

--- merge-bathy-lidar.py
+++ merge-bathy-lidar.py
@@ -4,7 +4,7 @@ lidar = pd.read_csv(r'M:\MGS\Coastal\BaltimoreCityReservoirs2020\Loch_Raven\LRgr
 bathy = pd.read_csv(r'M:\MGS\Coastal\BaltimoreCityReservoirs2020\Loch_Raven\LR_combined_bathy_xyz.csv')
 
 lidar["Depth"] = 239.96 - lidar["Elevation"] #subtract lidar elevation to MPL(NAVD88)
-lidarDF = lidar["Easting", "Northing", "Depth"].copy() #create copy of dataframe without elevation
+lidarDF = lidar[["Easting", "Northing", "Depth"]].copy() #create copy of dataframe without elevation
 lidarDF["Depth"] = lidarDF["Depth"] * 0.3048 #convert feet to meters
 
 dfs = [lidarDF, bathy] #add both datasets to list
